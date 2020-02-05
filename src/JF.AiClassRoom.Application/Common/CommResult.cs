6feb92b7f126e8cfd2a130dfaf6be992868a9efa
too <2279929737@qq.com>
2020-02-05 22:02:12 +0800

    new：添加通用数据返回类文件

---
+++ src/JF.AiClassRoom.Application/Common/CommResult.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JF.AiClassRoom.Common
+{
+    public class BaseCommResult
+    {
+        public ErrorCode Code { get; set; } = ErrorCode.Error;
+
+        /// <summary>
+        /// 提示消息
+        /// </summary>
+        public string Msg { get; set; }
+    }
+    public class CommResult : BaseCommResult
+    {
+        public dynamic Data { get; set; }
+    }
+
+    public class CommResult<T> : BaseCommResult
+    {
+        public T Data { get; set; }
+    }
+
+    public enum ErrorCode
+    {
+        /// <summary>
+        /// 成功
+        /// </summary>
+        Success = 10000,
+        /// <summary>
+        /// 操作失败
+        /// </summary>
+        Error = 00000,
+        /// <summary>
+        /// 认证失败
+        /// </summary>
+        AuthenticationFailed = 10001,
+        /// <summary>
+        /// 授权过期
+        /// </summary>
+        AuthenticationTimeOut = 10002,
+        /// <summary>
+        /// 参数不符合规范
+        /// </summary>
+        ParamVerificationFailed = 10003,
+        /// <summary>
+        /// 异常错误
+        /// </summary>
+        ExceptionError = 50003
+    }
+}
